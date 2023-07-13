431ecc089b8471d18d6dc5feeb0b2e09323ce514
mohammed Abate <71933125+moha-mame@users.noreply.github.com>
2023-07-13 11:34:21 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all bands with Glam as their main style, ranked by their longevity
 SELECT band_name,
-    YEAR('2022-01-01') - CAST(SUBSTRING_INDEX(split, ' - ', 1) AS SIGNED) AS lifespan
+    YEAR('2020-01-01') - CAST(SUBSTRING_INDEX(split, ' - ', 1) AS SIGNED) AS lifespan
 FROM metal_bands
 WHERE band_name IN (
     SELECT band_name
