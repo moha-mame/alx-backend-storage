45b7ca3e3b7be91b4f4f012908ec97bf95e6811a
moh-mame <moha.mame04@gmail.com>
2023-07-17 17:31:57 +0000

    MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- creates a view need_meeting that lists all students that have a
+DROP VIEW IF EXISTS need_meeting;
+
+CREATE VIEW need_meeting AS
+SELECT name 
+FROM students 
+WHERE score < 80 AND 
+(last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
